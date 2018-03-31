65f8bad76a0d3d77111fc4bd79cedefe4db661a8
Rohan Paul <rohanpaul2@gmail.com>
2018-03-31 13:25:56 +0530

    reverse string

---
+++ 10-days-js-solutions/day3/TryCatchFinally.js
@@ -0,0 +1,13 @@
+function reverseString(s) {
+  try {
+    s = s.split("").reverse().join("");
+
+  }
+  catch(e) {
+    console.log(e.message);
+  }
+  finally {
+    console.log(s);
+  }
+
+}
--- 10-days-js-solutions/day3/array-second-largest-element.js
+++ 10-days-js-solutions/day3/array-second-largest-element.js
@@ -8,4 +8,4 @@ function getSecondLargest(nums) {
   return sortedArrayUnique[sortedArrayUnique.length - 2];
 }
 
-// filter() calls a provided callback function once for each element in an array, and constructs a new array of all the values for which callback returns a value that coerces to true. So, in the above case, for the second occurance of the same element in the provided array, the .indexOf(element) will be different than the current index
+// filter() calls a provided callback function once for each element in an array, and constructs a new array of all the values for which callback returns a value that coerces to true. So, in the above case, for the second occurance of the same element in the providereturn sortedArrayUnique(sortedArrayUnique.length - 2) ;ud array, the .indexOf(element) will be different than the current index
